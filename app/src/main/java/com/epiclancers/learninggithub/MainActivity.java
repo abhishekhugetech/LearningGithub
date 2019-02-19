65a6a1c97982caaabb642c071813fdf4c00318ff
Abhishek Singh <35503790+abhishekhugetech@users.noreply.github.com>
2019-02-20 00:17:10 +0530

    Comment added to onCreate method

--- app/src/main/java/com/epiclancers/learninggithub/MainActivity.java
+++ app/src/main/java/com/epiclancers/learninggithub/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Change made to the Master Branch is here
     }
     
     public void newMethod(String data){
